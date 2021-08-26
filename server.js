ae46dcd0de4db11bbe5d1e6f92a3491db48a8cba
Wafi benjeddou <wafibenjeddou@gmail.com>
2021-08-26 16:23:10 +0200

    server configuration

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-const exprss =require('express');
-console.log('hey u are in ')
+const express =require('express');
+const app=express();
+const dotenv=require('dotenv')
+require('dotenv').config();
+app.listen(process.env.PORT,(error)=>{
+    if(error){
+        console.log(error)
+    }
+    console.log('the server has been started on port  '+process.env.PORT)
+})
+
